c23acafd86196acefe351df1d6c4ea19e010aa2c
Shibaz98 <shibaz98@gmail.com>
2024-05-18 14:59:58 +0100

    fixed bug where audio would not change

--- src/PreviewTrack/PreviewTrack.jsx
+++ src/PreviewTrack/PreviewTrack.jsx
@@ -7,8 +7,10 @@ const PreviewTrack = (props) =>{
     }
 
     let imageUrl = props.sample.album.images[0].url
-    let audioUrl = (props.sample.preview_url)
+    let audioUrl = props.sample.preview_url;
 
+    const key = audioUrl ? audioUrl : 'no-audio' // added this to the Audio tag to force the audio tag to re render, without it react will not re render the audio tag this is because audio will not re render on the basis of <source>'s audioUrl prop changing 
+    
     return(
         <div className='preview-container'>
             <div className="preview-information">
@@ -17,7 +19,7 @@ const PreviewTrack = (props) =>{
                     {props.track.artist} | {props.track.album}    
                 </p>
                 <img src={imageUrl}/>
-                <audio controls>
+                <audio key={key} controls>
                     <source src={audioUrl} type='audio/mpeg'/>
                 </audio>
             </div>
@@ -29,3 +31,5 @@ export default PreviewTrack;
 
 //
 // 
+
+
