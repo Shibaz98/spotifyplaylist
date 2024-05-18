69efe6a7c2502b992aa00d0b42700b5976cf401f
Shibaz98 <shibaz98@gmail.com>
2024-05-18 14:44:05 +0100

    succesfully added audio preview and audio controls

--- src/PreviewTrack/PreviewTrack.jsx
+++ src/PreviewTrack/PreviewTrack.jsx
@@ -7,15 +7,19 @@ const PreviewTrack = (props) =>{
     }
 
     let imageUrl = props.sample.album.images[0].url
+    let audioUrl = (props.sample.preview_url)
 
     return(
-        <div>
+        <div className='preview-container'>
             <div className="preview-information">
                 <h3>{props.track.name}</h3>
                 <p>
                     {props.track.artist} | {props.track.album}    
                 </p>
                 <img src={imageUrl}/>
+                <audio controls>
+                    <source src={audioUrl} type='audio/mpeg'/>
+                </audio>
             </div>
         </div>
     )
